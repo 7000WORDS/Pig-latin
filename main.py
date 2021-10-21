f9d517a0a3b5e4d9b691be4c887cc8d6067d3c50
words <60527853+7000WORDS@users.noreply.github.com>
2021-10-21 15:17:34 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+word = input("enter a word: ")
+vowels = ["a","e","i","o","u"]
+consonants = ["b","c","d","e","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if word in vowels:
+  print(word + "way")
+elif word in consonants:
+  print(word.replace())
